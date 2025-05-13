9a91c071063584bcf55cdf334cbfe14aa019cd71
shreyan01 <shreyanshukla19@gmail.com>
2025-05-13 20:09:55 +0530

    config files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,39 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-web3");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
     bscTestnet: {
-      url: process.env.BSC_TESTNET_RPC_URL,
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      gasPrice: 20000000000,
+      verify: {
+        etherscan: {
+          apiUrl: "https://api-testnet.bscscan.com"
+        }
+      }
     },
     bscMainnet: {
-      url: process.env.BSC_MAINNET_RPC_URL,
+      url: process.env.BSC_MAINNET_RPC_URL || "https://bsc-dataseed.binance.org/",
       chainId: 56,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      verify: {
+        etherscan: {
+          apiUrl: "https://api.bscscan.com"
+        }
+      }
     }
   },
   etherscan: {
